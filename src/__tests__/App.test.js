e7a558f1d9478cfee9de3d5024c229b4ce6e50a1
dkessler <dkessler@pillartechnology.com>
2020-04-29 07:10:49 -0500

    added acceptance tests for delete one and delete all

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "../App";
 
-test('should match the intial page render snapshot', () => {
-  const { container } = render(<App />)
+test("should match the intial page render snapshot", () => {
+  const { container } = render(<App />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test("should match after movie deletion", () => {
+  const { container } = render(<App />);
+  const deleteButtons = screen.getAllByText("Delete");
+
+  fireEvent.click(deleteButtons[0]);
+
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test("should match after all movies deleted", () => {
+  const { container } = render(<App />);
+  const deleteButtons = screen.getAllByText("Delete");
+
+  deleteButtons.forEach((deleteButton) => {
+    fireEvent.click(deleteButton);
+  });
+
   expect(container.firstChild).toMatchSnapshot();
 });
